e2833d242f21b9ff9f6bdcec5ab77f54459625fc
Rafael GR <105186822+RafaGR2972@users.noreply.github.com>
2023-11-04 20:50:49 -0300

    Update perseu.lua

--- Definitions/perseu.lua
+++ Definitions/perseu.lua
@@ -1,7 +1,7 @@
 local perseu = {}
 
 perseu.name = "Perseu"
-perseu.description = "Uma guerreira espadachin."
+perseu.description = "Herói conhecido por sua coragem e habilidades excepcionais, enfrentando desafios extraordinários e triunfando sobre as adversidades."
 perseu.maxHealth = 10
 perseu.health = 10
 perseu.attack = 4
