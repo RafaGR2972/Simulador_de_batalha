70f0ab8c30cc8ee7278ba8cd708a65f7de11bca3
Rafael GR <105186822+RafaGR2972@users.noreply.github.com>
2023-11-06 00:12:43 -0300

    Parametros

--- creature/medusa.lua
+++ creature/medusa.lua
@@ -3,8 +3,8 @@ local medusa = {}
 medusa.name = "Medusa"
 medusa.description = "Era uma terrível criatura de olhar petrificante e cabeleira formada por serpentes."
 medusa.maxHealth = 20
-medusa.health = 10
-medusa.attack = 8
+medusa.health = 20
+medusa.attack = 7
 medusa.defense = 6
 medusa.speed = 2
 
--- hero/perseu.lua
+++ hero/perseu.lua
@@ -3,9 +3,9 @@ local perseu = {}
 perseu.name = "Perseu"
 perseu.description = "Herói conhecido por sua coragem e habilidades excepcionais, enfrentando desafios extraordinários e triunfando sobre as adversidades."
 perseu.maxHealth = 10
-perseu.health = 5
+perseu.health = 10
 perseu.attack = 4
-perseu.defense = 2
+perseu.defense = 3
 perseu.speed = 4
 perseu.item = {"Elmo", "Sandálias aladas", "Escudo"}
 
